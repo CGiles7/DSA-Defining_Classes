487c0ffa9780f2a80052949bdd446bc79e4cdc5c
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-04 14:22:15 -0400

    flexible classes

--- practice.js
+++ practice.js
@@ -1,11 +1,23 @@
 class Event {
-    constructor(name, startTime, endTime, location) {
-      this.name = name;
-      this.startTime = startTime;
-      this.endTime = endTime;
-      this.location = location;
+    constructor(name, attendees, startTime, locationName, address) {
+      let guests = null;
+      if (attendees.length === 1) {
+        guests = attendees[0];
+      } else if (attendees.length > 1) {
+        guests = `${attendees.length} attendees`;
+      }
+  
+      this.name = guests ? `${name} with ${guests}` : name;
+      this.details = { startTime, attendees, groupSize: attendees.length };
+      this.location = { name: locationName, address };
     }
   }
-
-  const lunch = new Event("Lunch", "12:00", "13:00", "Chipotle");
-console.log(lunch);
+  
+  const event = new Event("Dinner", ["Sal"], 1830, "Frankie's", "477 Rain St.");
+  /*
+    Event {
+      name: 'Dinner with Sal',
+      details: { startTime: 1830, attendees: [ 'Sal' ], groupSize: 1 },
+      location: { name: "Frankie's", address: '477 Rain St.' }
+    }
+  */
