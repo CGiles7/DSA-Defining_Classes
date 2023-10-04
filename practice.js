10e154e14bbec4c8b0cf16a83411551c8151108b
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-04 14:25:16 -0400

    defining, calling a method

--- practice.js
+++ practice.js
@@ -1,23 +1,15 @@
 class Event {
-    constructor(name, attendees, startTime, locationName, address) {
-      let guests = null;
-      if (attendees.length === 1) {
-        guests = attendees[0];
-      } else if (attendees.length > 1) {
-        guests = `${attendees.length} attendees`;
+    constructor(name, startTime, endTime, location) {
+      this.name = name;
+      this.startTime = startTime;
+      this.endTime = endTime;
+      this.location = location;
+    }
+    toString() {
+        const { name, startTime, endTime, location } = this;
+        return `${startTime} - ${endTime}: ${name} at ${location}`;
       }
-  
-      this.name = guests ? `${name} with ${guests}` : name;
-      this.details = { startTime, attendees, groupSize: attendees.length };
-      this.location = { name: locationName, address };
-    }
   }
-  
-  const event = new Event("Dinner", ["Sal"], 1830, "Frankie's", "477 Rain St.");
-  /*
-    Event {
-      name: 'Dinner with Sal',
-      details: { startTime: 1830, attendees: [ 'Sal' ], groupSize: 1 },
-      location: { name: "Frankie's", address: '477 Rain St.' }
-    }
-  */
+
+  const lunchEvent = new Event("Lunch", "12:00", "13:00", "Chipotle");
+console.log(lunchEvent.toString()); // > 12:00 - 13:00: Lunch at Chipotle
